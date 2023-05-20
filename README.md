5599b9ebb715779a99fc878d4571121f0dceec8c
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-05-20 09:46:59 +0200

    refactor: minor rewrite and cleanup

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # composer-local-repo-plugin
 
-A plugin for [Composer][composer website].
+This plugin for [Composer][composer website] allows to create a local repository
+of type "composer" ([see documentation][composer repository]) from an existing
+package.
+
+This is useful when you want to install a Composer package without network
+access.
+
+This project is heavily inspired by
+[`fossar/composition-c4`][fossar/composition-c4] from [Jan Tojnar][Jan Tojnar].
 
 ## Requirements
 
 - Composer 2
-- PHP 
 
-## Usage
+## Installation
 
 Install the plugin globally once:
 
@@ -15,16 +22,20 @@ Install the plugin globally once:
 composer global require loophp/composer-local-repo-plugin
 ```
 
-Then, for each project where you want to create a local composer repository, do:
+## Usage
 
-```
-composer build-local-repo /path/to/local/repository
-composer config repo.packagist false
-composer config repo.local '{"type": "composer", "url": "file:///path/to/local/reposity"}'
-composer update --lock --no-install
-composer install
-```
+Create a local composer repository, for an existing package:
 
-This project is inspired by `fossar/composition-c4` at https://github.com/fossar/composition-c4/
+1. Move into the package directory (*an option will be provided in the future to skip this step*)
+2. Make sure the `composer.json` and `composer.lock` files are present.
+3. Build the local repository: `composer build-local-repo /path/to/local/repository`
+4. Disable download from `packagist.org` repository: `composer config repo.packagist false`
+5. Add the new local `composer` repository to the `composer.json` file: `composer config repo.local '{"type": "composer", "url": "file:///path/to/local/reposity"}'`
+6. At this point you can disable network, no network access is needed any more
+7. Update the lock file: `composer update --lock --no-install`
+8. Install the package: `composer install`
 
 [composer website]: https://getcomposer.org/
+[fossar/composition-c4]: https://github.com/fossar/composition-c4/
+[Jan Tojnar]: https://github.com/jtojnar
+[composer repository]: https://getcomposer.org/doc/05-repositories.md#composer
--- src/Command/BuildLocalRepo.php
+++ src/Command/BuildLocalRepo.php
@@ -8,13 +8,14 @@ use Composer\Command\BaseCommand;
 use Composer\Json\JsonFile;
 use Composer\Package\CompletePackage;
 use Composer\Package\Loader\ArrayLoader;
+use Composer\Package\Locker;
 use Composer\Util\Filesystem;
+use Exception;
 use Generator;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
 
 final class BuildLocalRepo extends BaseCommand
 {
@@ -22,9 +23,8 @@ final class BuildLocalRepo extends BaseCommand
     {
         $this
             ->setName('build-local-repo')
-            ->setDescription('Create local composer repositories for offline use')
-            ->addArgument('repo-dir', InputArgument::REQUIRED, 'Target directory to create repo in')
-            ->addOption('no-dev', null, InputOption::VALUE_NONE, 'Disables installation of require-dev packages.');
+            ->setDescription('Create local repositories with type "composer" for offline use.')
+            ->addArgument('repo-dir', InputArgument::REQUIRED, 'Target directory to create repo in');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -32,10 +32,27 @@ final class BuildLocalRepo extends BaseCommand
         $composer = $this->requireComposer(true, true);
         $downloadManager = $composer->getDownloadManager();
         $fs = new Filesystem();
+        $repoDir = $input->getArgument('repo-dir');
+
+        if (false === realpath($repoDir)) {
+            throw new Exception('Repository path directory does not exist.');
+        }
+
+        $locker = $this->requireComposer(true, true)->getLocker();
+
+        if (false === $locker->isLocked()) {
+            throw new Exception('Composer lock file does not exist.');
+        }
 
         $packages = [];
-        foreach ($this->iterLockedPackages($input) as [$packageInfo, $package]) {
-            $version = $package->getVersion();
+        foreach ($this->iterLockedPackages($locker) as [$packageInfo, $package]) {
+            unset($packageInfo['source']);
+            $version = $packageInfo['version'];
+            $reference = $packageInfo['dist']['reference'];
+            $name = $packageInfo['name'];
+            $packagePath = sprintf('%s/%s/%s', $repoDir, $name, $version);
+            $downloadManager = $downloadManager->setPreferSource(true);
+
             // While Composer repositories only really require `name`, `version` and `source`/`dist` fields,
             // we will use the original contents of the package’s entry from `composer.lock`, modifying just the sources.
             // Package entries in Composer repositories correspond to `composer.json` files [1]
@@ -46,42 +63,34 @@ final class BuildLocalRepo extends BaseCommand
             //     "PathDownloader" is a dist type downloader and can not be used to download source
             //
             // [1]: https://getcomposer.org/doc/05-repositories.md#packages>
-            unset($packageInfo['source']);
-
-            $packages[$package->getPrettyName()] = [
-                $version => [
-                    'dist' => [
-                        'reference' => $package->getDistReference(),
-                        'type' => 'path',
-                        'url' => sprintf('%s/%s/%s', $input->getArgument('repo-dir'), $package->getName(), $version),
-                    ],
-                ] + $packageInfo,
-            ];
+            $packages[$name][$version] = [
+                'dist' => [
+                    'reference' => $reference,
+                    'type' => 'path',
+                    'url' => $packagePath,
+                ],
+            ] + $packageInfo;
 
             $downloadManager
-                ->setPreferSource(true)
                 ->download(
                     $package,
-                    sprintf('%s/%s/%s', $input->getArgument('repo-dir'), $package->getName(), $version),
+                    $packagePath,
                 );
 
             $downloadManager
-                ->setPreferSource(true)
                 ->install(
                     $package,
-                    sprintf('%s/%s/%s', $input->getArgument('repo-dir'), $package->getName(), $version),
+                    $packagePath,
                 )
                 ->then(
-                    static function () use ($fs, $input, $package, $version): void {
-                        $fs->removeDirectory(sprintf('%s/%s/%s/.git', $input->getArgument('repo-dir'), $package->getName(), $version));
-                    }
+                    static fn (): bool => $fs->removeDirectory(sprintf('%s/.git', $packagePath))
                 );
         }
 
-        (new JsonFile(sprintf('%s/packages.json', $input->getArgument('repo-dir'))))->write(['packages' => $packages]);
+        (new JsonFile(sprintf('%s/packages.json', $repoDir)))->write(['packages' => $packages]);
 
         $output->writeln(
-            sprintf('Local composer repository has been successfully created in %s', $input->getArgument('repo-dir'))
+            sprintf('Local composer repository has been successfully created in %s', $repoDir)
         );
 
         return Command::SUCCESS;
@@ -90,14 +99,8 @@ final class BuildLocalRepo extends BaseCommand
     /**
      * @return Generator<int, CompletePackage>
      */
-    private function iterLockedPackages(InputInterface $input): Generator
+    private function iterLockedPackages(Locker $locker): Generator
     {
-        $locker = $this->requireComposer(true, true)->getLocker();
-
-        if ($locker->isLocked() === false) {
-            return;
-        }
-
         $data = $locker->getLockData();
         $loader = new ArrayLoader(null, true);
 
@@ -105,10 +108,8 @@ final class BuildLocalRepo extends BaseCommand
             yield [$info, $loader->load($info)];
         }
 
-        if (false === $input->getOption('no-dev')) {
-            foreach ($data['packages-dev'] ?? [] as $info) {
-                yield [$info, $loader->load($info)];
-            }
+        foreach ($data['packages-dev'] ?? [] as $info) {
+            yield [$info, $loader->load($info)];
         }
     }
 }
