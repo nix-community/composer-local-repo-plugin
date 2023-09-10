0b91eb1e6aad11a9845455722fe6a10a78c80545
Pol Dellaiera <pol.dellaiera@protonmail.com>
2023-09-10 10:28:20 +0200

    now rely on code from Composer ^2.6
    Context: https://github.com/composer/composer/pull/11496

--- src/Service/RepoBuilder.php
+++ src/Service/RepoBuilder.php
@@ -5,12 +5,8 @@ declare(strict_types=1);
 namespace loophp\ComposerLocalRepoPlugin\Service;
 
 use Composer\Composer;
-use Composer\Downloader\DownloadManager;
 use Composer\Package\Loader\ArrayLoader;
-use Composer\Package\PackageInterface;
-use Composer\Util\Loop;
-use Exception;
-use React\Promise\PromiseInterface;
+use Composer\Util\SyncHelper;
 
 final class RepoBuilder extends LocalBuilder
 {
@@ -21,52 +17,12 @@ final class RepoBuilder extends LocalBuilder
         $downloadManager = $composer->getDownloadManager();
 
         foreach ($this->iterLockedPackages($composer->getLocker(), $includeDevDeps) as $packageInfo) {
-            $this->downloadAndInstallPackageSync(
+            SyncHelper::downloadAndInstallPackageSync(
                 $loop,
                 $downloadManager,
                 sprintf('%s/%s/%s', $destinationDir, $packageInfo['name'], $packageInfo['version']),
                 $loader->load($packageInfo)
             );
         }
-    }
-
-    /**
-     * @param null|PromiseInterface<mixed> $promise
-     */
-    private function await(Loop $loop, ?PromiseInterface $promise = null): void
-    {
-        if (null !== $promise) {
-            $loop->wait([$promise]);
-        }
-    }
-
-    /**
-     * Shamelessly copied from https://github.com/composer/composer/blob/52f6f74b7c342f7b90be9c1a87e183092e8ab452/src/Composer/Util/SyncHelper.php.
-     *
-     * Uses the `DownloaderManager` instead of a `Downloader` to download and install a package.
-     *
-     * TODO: this could be removed at the next version of Composer, see https://github.com/composer/composer/pull/11496
-     */
-    private function downloadAndInstallPackageSync(Loop $loop, DownloadManager $downloadManager, string $path, PackageInterface $package, ?PackageInterface $prevPackage = null): void
-    {
-        $type = null !== $prevPackage ? 'update' : 'install';
-
-        try {
-            $this->await($loop, $downloadManager->download($package, $path, $prevPackage));
-
-            $this->await($loop, $downloadManager->prepare($type, $package, $path, $prevPackage));
-
-            if ('update' === $type && null !== $prevPackage) {
-                $this->await($loop, $downloadManager->update($package, $prevPackage, $path));
-            } else {
-                $this->await($loop, $downloadManager->install($package, $path));
-            }
-        } catch (Exception $e) {
-            $this->await($loop, $downloadManager->cleanup($type, $package, $path, $prevPackage));
-
-            throw $e;
-        }
-
-        $this->await($loop, $downloadManager->cleanup($type, $package, $path, $prevPackage));
     }
 }
